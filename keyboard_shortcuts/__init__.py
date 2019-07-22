60b835cf285323921c5ad37f65890f5bf60021bc
AJ Bowen <aj@soulshake.net>
2019-07-22 11:13:01 +0200

    Rewrite exception handling for python3 compatibility

--- keyboard_shortcuts/__init__.py
+++ keyboard_shortcuts/__init__.py
@@ -1,7 +1,10 @@
-__all__ = ('VERSION',)
+__all__ = ("VERSION",)
 
 try:
-    VERSION = __import__('pkg_resources') \
-        .get_distribution('django-keyboard-shortcuts').version
-except Exception, e:
-    VERSION = 'unknown'
+    VERSION = (
+        __import__("pkg_resources")
+        .get_distribution("django-keyboard-shortcuts")
+        .version
+    )
+except Exception as e:
+    VERSION = "unknown"
